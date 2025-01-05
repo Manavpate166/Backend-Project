e61f82cd9b134c9fe882029d00abfdf46e8bbfff
manav <patelmanav166@gmail.com>
2025-01-05 11:42:43 +0530

    Connect to mongo Db

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js" 
+
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+/*
+(async()=>{
+    try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    app.on("error",(error)=>{
+        console.log("Error in database connection",error);
+        throw error
+    })
+
+    app.listen(process.env.PORT,()=>{
+        console,log(`app is listening on port numner: ${process.env.PORT}`)
+    })
+    } catch (error) {
+        console.error("Error :",error);
+        throw err
+        
+    }
+})()
+*/
