3413f9f353fff90147ecfc06c8d52ca2614236ce
manav <patelmanav166@gmail.com>
2025-01-08 12:59:14 +0530

    added usermodel and videomodel

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import connectDB from "./db/index.js"
 dotenv.config({
     path:'./env'
 })
-connectDB()
+connectDB().then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log("server is running on port",process.env.PORT)
+    })
+}).catch((error)=>{
+    console.log(`Mongo db connection failed!!!:- ${error}`);
+})
 /*
 (async()=>{
     try {
